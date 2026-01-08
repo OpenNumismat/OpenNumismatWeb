e87da6c910f5c3b71949239ca5a6664112c180fc
JanisV <ignatov.vit@gmail.com>
2026-01-08 18:08:58 +0300

    Improve settings page

--- frontend/src/components/SettingsView.vue
+++ frontend/src/components/SettingsView.vue
@@ -38,44 +38,55 @@ const handleThemeChange = (theme) => {
 <template>
   <v-container>
     <v-list>
-      <v-list-item>
-        <v-btn-toggle
-            v-model="themeStore.currentTheme"
-            rounded="xl"
-            border
-            @update:model-value="handleThemeChange"
-        >
-          <v-btn value="dark" icon="mdi-weather-night"></v-btn>
-          <v-btn value="system" icon="mdi-brightness-auto"></v-btn>
-          <v-btn value="light" icon="mdi-weather-sunny"></v-btn>
-        </v-btn-toggle>
+      <v-list-item :title="i18n.global.t('settings_theme')">
+        <template v-slot:append>
+          <v-list-item-action start>
+            <v-btn-toggle
+                v-model="themeStore.currentTheme"
+                rounded="xl"
+                border
+                @update:model-value="handleThemeChange"
+            >
+              <v-btn value="dark" icon="mdi-weather-night"></v-btn>
+              <v-btn value="system" icon="mdi-brightness-auto"></v-btn>
+              <v-btn value="light" icon="mdi-weather-sunny"></v-btn>
+            </v-btn-toggle>
+          </v-list-item-action>
+        </template>
       </v-list-item>
-      <v-list-item>
-        <v-select
-            v-model="$i18n.locale"
-            :items="languageItems"
-            item-title="name"
-            item-value="lang"
-            @update:model-value="setLocale"
-        >
-        </v-select>
+      <v-list-item :title="i18n.global.t('settings_language')">
+        <template v-slot:append>
+          <v-list-item-action start>
+            <v-select
+                v-model="$i18n.locale"
+                :items="languageItems"
+                item-title="name"
+                item-value="lang"
+                @update:model-value="setLocale"
+            >
+            </v-select>
+          </v-list-item-action>
+        </template>
       </v-list-item>
-      <v-list-item>
-        <v-btn-toggle
-            v-model="statusStore.currentStatusView"
-            rounded="xl"
-            border
-        >
-          <v-btn
-              v-for="item in statusItems" :key="item.value" :value="item.value">
-            {{ i18n.global.t(item.title) }}
-          </v-btn>
-        </v-btn-toggle>
+      <v-list-item :title="i18n.global.t('settings_status')">
+        <template v-slot:append>
+          <v-list-item-action start>
+            <v-btn-toggle
+                v-model="statusStore.currentStatusView"
+                rounded="xl"
+                border
+            >
+              <v-btn
+                  v-for="item in statusItems" :key="item.value" :value="item.value">
+                {{ i18n.global.t(item.title) }}
+              </v-btn>
+            </v-btn-toggle>
+          </v-list-item-action>
+        </template>
       </v-list-item>
     </v-list>
   </v-container>
 </template>
 
 <style scoped>
-
 </style>
