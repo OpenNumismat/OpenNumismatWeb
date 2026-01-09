bf95760ef9e8ec3a492640525ac89b6fb8320d16
JanisV <ignatov.vit@gmail.com>
2026-01-10 00:43:54 +0300

    Add PWA

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 
+import { VitePWA } from 'vite-plugin-pwa'
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -10,6 +11,19 @@ export default defineConfig(({ mode }) => {
     base: env.VITE_BASE_PATH || '/',
     plugins: [
       vue(),
+      VitePWA({
+        registerType: 'autoUpdate',
+        manifest: {
+          name: 'OpenNumismat Web',
+          short_name: 'OpenNumismat',
+          background_color: '#FFFFFF',
+          theme_color: '#FFA726',
+          icons: [
+            { src: 'icon-192.png', sizes: '192x192', type: 'image/png' },
+            { src: 'icon-512.png', sizes: '512x512', type: 'image/png' }
+          ]
+        }
+      }),
     ],
     resolve: {
       alias: {
