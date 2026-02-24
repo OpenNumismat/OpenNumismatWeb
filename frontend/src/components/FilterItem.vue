37ee8d77ee1e9068a3c14607f36b9112f04e3149
Janis <opennumismat@gmail.com>
2026-02-24 11:30:29 +0300

    Close filter menu after select All

--- frontend/src/components/FilterItem.vue
+++ frontend/src/components/FilterItem.vue
@@ -1,15 +1,19 @@
 <script setup>
+import {ref} from "vue";
 import i18n from "@/i18n/index.js";
 
 const props = defineProps(['filters', 'field', 'settings'])
 const emit = defineEmits(['filterChanged'])
 const selectedFilter = defineModel()
 
+const isMenuOpen = ref(false)
+
 const onFilterChanged = async (val) => {
   emit('filterChanged', props.field, val)
 }
 
 const onFilterClear = async () => {
+  isMenuOpen.value = false
   selectedFilter.value = null
   emit('filterChanged', props.field, null)
 }
@@ -23,6 +27,7 @@ const onFilterClear = async () => {
     :item-title="item => settings.statuses[item]"
     @update:modelValue="onFilterChanged"
     return-object
+    v-model:menu="isMenuOpen"
   >
     <template v-slot:prepend-item>
       <v-list-item :title="i18n.global.t('All')" @click="onFilterClear"></v-list-item>
