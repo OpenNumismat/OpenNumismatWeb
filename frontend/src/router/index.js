f241838bd0577131445fe85a08bb2367c8655d68
Janis <opennumismat@gmail.com>
2026-03-30 16:48:27 +0300

    Fix router warnings

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,30 +4,37 @@ const routes = [
   {
     path: '/',
     name: 'home',
+    component: { render: () => null },
   },
   {
     path: '/coin/:id',
     name: 'coin',
+    component: { render: () => null },
   },
   {
     path: '/images/:id',
     name: 'images',
+    component: { render: () => null },
   },
   {
     path: '/open',
     name: 'open',
+    component: { render: () => null },
   },
   {
     path: '/settings',
     name: 'settings',
+    component: { render: () => null },
   },
   {
     path: '/summary',
     name: 'summary',
+    component: { render: () => null },
   },
   {
     path: '/about',
     name: 'about',
+    component: { render: () => null },
   },
 ]
 
