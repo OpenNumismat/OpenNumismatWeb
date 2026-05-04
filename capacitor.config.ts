6eed49b9bf125eb03fe290039dccafcff684bf67
Janis <opennumismat@gmail.com>
2026-05-04 17:31:16 +0300

    Add Capacitor build

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'org.opennumismat.OpenNumismatWeb',
+  appName: 'OpenNumismat',
+  webDir: 'frontend/dist',
+  bundledWebRuntime: false
+};
+
+export default config;
